6c694d1f8910a855ea3d8073aa218f37e84ddd78
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-01-02 12:05:31 -0700

    apiCalls

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -4,12 +4,11 @@ import { publicRequest } from "../requestMethods";
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
   try {
-    const res = await publicRequest.post(user);
+    const res = await publicRequest.post("/auth/login", user);
     dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
   }
 };
-
 
 
