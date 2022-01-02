47bfa1fd4c6a6dab93133a8e4f671031c2a5607d
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-01-02 10:11:14 -0700

    find func update

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -132,7 +132,7 @@ const Product = () => {
   useEffect(() => {
     const getProduct = async () => {
       try {
-        const res = await publicRequest.get("/products/find/" + id);
+        const res = await publicRequest.get("/api/products/find/" + id);
         setProduct(res.data);
       } catch {}
     };
