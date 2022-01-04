c3e2e27e6c3a48bb3154b87a3b929ffc222e02b8
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-01-03 21:59:00 -0700

    updated shop modal

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -253,7 +253,7 @@ const Cart = () => {
               <SummaryItemPrice>$ {cart.total}</SummaryItemPrice>
             </SummaryItem>
             <StripeCheckout
-              name="Lama Shop"
+              name="SWAG."
               image="https://avatars.githubusercontent.com/u/1486366?v=4"
               billingAddress
               shippingAddress
